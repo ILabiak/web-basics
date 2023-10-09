ab2a3cfa0a409f1b2452c7239a1598ef00e4bbf2
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-10-09 14:33:33 +0300

    add lab4

---
+++ lab4/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const users = {};
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('new user', (username) => {
+    users[socket.id] = username;
+  });
+
+  socket.on('chat message', (data) => {
+    io.emit('chat message', {
+      username: users[socket.id],
+      message: data.message,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
